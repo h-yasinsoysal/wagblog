fbf85308d57e7418894dbdd07bfd866dd22c3e75
Yasin <lider@Yasin-MacBook-Pro.local>
2021-06-03 17:17:43 +0300

    Initial commit

---
+++ wagblog/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-#d_hgi24y&eo347h&9igm%1a9%emm*6z#)#z0ee3yklj$5+%v_'
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ['*'] 
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
